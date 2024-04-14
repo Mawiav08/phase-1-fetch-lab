75870d38c598ea9ce588836fe0ec067e13a20101
mawiav08 <mawiav008@gmail.com>
2024-04-14 19:36:34 +0300

    complete labs

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(res =>res.json())
+  .then(json => renderBooks(json));
 }
-
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  books.forEach(book=>{
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
@@ -13,5 +14,15 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  fetchBooks()
+  .then(response =>{
+    if(!response.ok){
+      throw new Error('Failed to fetch books')
+    }
+    return response.json();
+  })
+  .then(renderBooks)
+  .catch(error =>{
+    console.error('Error rendering books', error);
+  });
 });
